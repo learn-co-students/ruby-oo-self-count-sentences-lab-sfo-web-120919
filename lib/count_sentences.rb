8af1f6aaaba6e8fb0275a7034844e2d8c3db0a86
github username <github email address>
2019-12-11 22:20:01 -0800

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # write an expression to compare "." with the last character of self
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    # turn string into an array of sentences
+    return self.split(/[.!?]+/).count
   end
 end
